491c2837993696512d1e5a7713e255aff03f149f
Yuvraj <agrahariraunak854@gmail.com>
2026-09-10 05:51:50 +0530

    Add JavaScript comparison practice

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+// in JS comparison & equality check are 2 different things.
+
+// === strict check checks value as well as datatype
+
+console.log("2" === 2);
+// clean code is valued more & it should be used in most of the cases.
